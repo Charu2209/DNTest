7982b53ffee7c376debbeeb9b7fdc93d7fd06c4e
Vishal Negi <tropbon9@gmail.com>
2024-01-21 11:34:11 +0000

    using request body

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask,render_template,request
 #import pickle
 import main as m
-#import json
+import json
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/',methods=['GET'])
 def new():
     return render_template('new.html')
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/',methods=['POST'])
 def predict():
-    data1 = (request.form['text1'])
-    data2 = (request.form['text2'])
+    # data1 = (request.form['text1'])
+    # data2 = (request.form['text2'])
 
-    #data = request.json
-    #data1=data.get('text1')
-    #data2 = data.get('text2')
+    data = request.json
+    data1 = data.get('text1')
+    data2 = data.get('text2')
     
    # model=pickle.load(open('model.pickle','rb'))
     data = m.tagged_data(data1,data2)
@@ -25,9 +25,10 @@ def predict():
     
     prediction_statement = f"Similarity Score is:{pred}"
 
-    #output = {"similarity score":pred}
-    return render_template('new.html',statement=prediction_statement)
-    #return json.dumps(output)
+    output = {"similarity score": pred}
+    print(pred)
+    # return render_template('new.html',statement=prediction_statement)
+    return json.dumps(output)
 
 
 if __name__=='__main__':
